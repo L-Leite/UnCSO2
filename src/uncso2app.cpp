b7f590aa020230ce3456602c09f6d21a2847fc6f
Luís Leite <ochii@leite.xyz>
2019-09-25 20:24:01 +0100

    Move source files to new locations

--- src/uncso2app.cpp
+++
@@ -1,8 +0,0 @@
-#include "stdafx.h"
-#include "uncso2app.h"
-
-CUnCSO2App::CUnCSO2App( int& argc, char** argv ) : QApplication( argc, argv )
-{
-	m_pMainWindow = new CMainWindow();
-	m_pMainWindow->show();
-}
--- src/version.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "gitversion.h"
-#include <QString>
-
-#define APP_VERSION "1.4"
-
-QString GetCurrentCommit();
-QString GetAppVersion();
