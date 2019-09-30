bffce307246117a1d8e42c22d2767a87ce8f2336
Luís Leite <ochii@leite.xyz>
2019-09-30 22:37:01 +0100

    Add CI scripts and config

---
+++ ci/pack.ps1
@@ -0,0 +1,102 @@
+function CreateDirectory {
+    param( [string]$newDirectory)
+    New-Item -ItemType Directory -Path $newDirectory
+}
+
+$curBuildCombo = $env:BUILD_COMBO
+$curConfig = $env:CONFIGURATION
+
+$isMingwBuild = $curBuildCombo -eq 'windows-mingw'
+$isMsvcBuild = $curBuildCombo -eq 'windows-msvc'
+
+Write-Host "Running packaging script..."
+Write-Host "Current setup build combo is: $curBuildCombo"
+
+# create dir to store package files
+CreateDirectory ./build/package
+
+if ($isLinux) {
+    # copy libuncso2 to the package dir
+    Copy-Item ./build/libuncso2/libuncso2.so* -Destination ./build/package/
+
+    # copy libcryptopp to the package dir
+    Copy-Item ./build/libuncso2/external/cryptopp/libcryptopp.so* -Destination ./build/package/
+
+    # copy AppImage prebuilt files
+    Copy-Item ./appimage/* -Destination ./build/package/
+
+    # copy uncso2 itself to the package dir
+    Copy-Item ./build/uc2 ./build/package/
+}
+elseif ($isWindows) {
+    if ($isMingwBuild) {
+        # copy libcryptopp to the same directory of uc2
+        cp ./build/libuncso2/libuncso2.dll ./build/package/
+
+        # copy libuncso2 to the same directory of uc2
+        cp ./build/libuncso2/external/cryptopp/libcryptopp.dll ./build/package/
+    }
+    elseif ($isMsvcBuild) {
+        # copy libcryptopp to the same directory of uc2
+        cp ./build/libuncso2/uncso2.dll ./build/package/
+    }
+    else {        
+        Write-Error 'Unknown build combo detected.'
+        exit 1
+    }
+    
+    # copy uncso2 itself to the package dir
+    Copy-Item ./build/uc2.exe ./build/package/
+}
+else {
+    Write-Error 'An unknown OS is running this script, implement me.'
+    exit 1
+}
+
+# copy breeze icons
+Copy-Item ./build/icons-breeze.rcc -Destination ./build/package/
+
+# get app version
+$versionStr = Get-Content -Path ./version.txt -TotalCount 1
+Write-Host "UnCSO2 version: $versionStr"
+
+Push-Location ./build
+Push-Location ./package
+
+if ($isLinux) {
+    # retrieve deployment tool
+    wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+    chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+
+    $env:VERSION = $versionStr;
+    ./linuxdeployqt-continuous-x86_64.AppImage ./uc2 -bundle-non-qt-libs -appimage
+}
+elseif ($isWindows) {
+    $windeployBin = ''
+
+    if ($isMingwBuild) {
+        $windeployBin = 'C:\Qt\5.13\mingw73_64\windeployqt.exe'
+    }
+    elseif ($isMsvcBuild) {        
+        $windeployBin = 'C:\Qt\5.13\msvc2017_64\windeployqt.exe'
+    }
+    else {        
+        Write-Error 'Unknown build combo detected.'
+        exit 1
+    }
+
+    if ($curConfig -eq 'Release') {       
+        & $windeployBin --release ./uc2.exe  
+    }
+    else {       
+        & $windeployBin ./uc2.exe  
+    }  
+}
+
+Pop-Location
+
+if ($isWindows) {
+    7z a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=64m -ms=on "UnCSO2-$versionStr-win64.7z" ./package/*
+}
+
+Pop-Location
---
+++ ci/setup.ps1
@@ -0,0 +1,139 @@
+function SetupVsToolsPath {
+    # from https://allen-mack.blogspot.com/2008/03/replace-visual-studio-command-prompt.html
+
+    # split location to shorten the command
+    Push-Location 'C:\Program Files (x86)\Microsoft Visual Studio\2017'
+    Push-Location '.\Community\VC\Auxiliary\Build'
+
+    cmd /c "vcvars64.bat&set" |
+    ForEach-Object {
+        if ($_ -match "=") {
+            $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+        }
+    }
+
+    Pop-Location
+    Pop-Location
+}
+
+function PrintToolsVersion {
+    param ([string]$curBuildCombo)
+
+    Write-Host '#'
+    Write-Host '# TOOLS VERSIONS'
+    Write-Host '#'
+
+    switch ($curBuildCombo) {
+        "linux-gcc" {
+            which gcc-9
+            Write-Host '# GCC'
+            gcc-9 -v
+            break
+        }
+        "linux-clang" {
+            which clang-8
+            Write-Host '# Clang - GCC/Linux'
+            clang-8 -v
+            break
+        }
+        "windows-mingw" {
+            Write-Host '# MinGW'
+            C:\msys64\mingw64\bin\gcc.exe -v
+            break
+        }
+        "windows-msvc" {
+            which cl
+            Write-Host '# MSVC'
+            cl
+            break
+        }
+        Default {
+            Write-Error 'Unknown build combo used, could not find appropriate compiler.'
+            exit 1
+        }
+    }
+    
+    Write-Host '# CMake'
+    cmake --version
+
+    Write-Host '# Ninja'
+    ninja --version
+
+    Write-Host '# Git'
+    git --version
+
+    Write-Host '#'
+    Write-Host '# END OF TOOLS VERSIONS'
+    Write-Host '#'
+}
+
+$curBuildCombo = $env:BUILD_COMBO
+
+#$isGccBuild = $curBuildCombo -eq 'linux-gcc' # unused
+$isLinuxClangBuild = $curBuildCombo -eq 'linux-clang'
+$isMingwBuild = $curBuildCombo -eq 'windows-mingw'
+$isMsvcBuild = $curBuildCombo -eq 'windows-msvc'
+
+Write-Host "Running setup script..."
+Write-Host "Current setup build combo is: $curBuildCombo"
+
+if ($isLinux) {
+    # install ninja through apt
+    sudo apt install ninja-build
+
+    # install qt
+    sudo add-apt-repository "ppa:beineri/opt-qt-5.12.3-xenial"
+    sudo apt-get update
+    sudo apt qt512base qt512svg
+
+    if ($isLinuxClangBuild) {
+        # retrieve clang 8
+        sudo add-apt-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-8 main"
+        sudo apt update
+        sudo apt install -y clang-8 lldb-8 lld-8 libc++-8-dev libc++abi-8-dev
+    }
+}
+elseif ($isWindows) {
+    # install scoop
+    Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression
+
+    # install ninja through scoop
+    scoop install ninja
+
+    if ($isMingwBuild) {
+        # put mingw tools in path
+        $mingwAppendPath = ';C:\msys64\mingw64\bin'
+        $env:Path += $mingwAppendPath
+        [Environment]::SetEnvironmentVariable("Path", $env:Path + $mingwAppendPath, "Machine")
+
+        # put Qt in CMake prefix path
+        $mingwPrefixPath = 'C:\Qt\5.13.0\mingw73_64'
+        $env:CMAKE_PREFIX_PATH += $mingwPrefixPath
+        [Environment]::SetEnvironmentVariable("CMAKE_PREFIX_PATH", $mingwPrefixPath, "Machine")
+    }
+
+    if ($isMsvcBuild) {     
+        # put VS tools in path to print their version
+        SetupVsToolsPath
+
+        # put Qt in CMake prefix path
+        $msvcPrefixPath = 'C:\Qt\5.13.0\msvc2017_64'
+        $env:CMAKE_PREFIX_PATH += $msvcPrefixPath
+        [Environment]::SetEnvironmentVariable("CMAKE_PREFIX_PATH", $msvcPrefixPath, "Machine")
+    }
+}
+else {
+    Write-Error 'An unknown OS is running this script, implement me.'
+    exit 1
+}
+
+Write-Host '#'
+Write-Host '# Environment path:'
+Write-Host '#'
+Write-Host $env:PATH
+
+# print tools versions
+PrintToolsVersion $curBuildCombo
+
+# setup submodules
+git submodule update --init --recursive
