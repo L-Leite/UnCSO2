d8f878c1cbb122a996100941869b0a4a6ddc19cb
Luís Leite <ochii@leite.xyz>
2019-10-06 20:32:28 +0100

    Set Titanfall Online flag when creating a PkgFile object for TFO.
    Fixes loading PKG files from an index file

--- sources/dynpkgfilefactory.cpp
+++ sources/dynpkgfilefactory.cpp
@@ -170,9 +170,20 @@ bool DynamicPkgFileFactory::TrySpecificProvider( GameProvider provider )
     auto [entryKey, dataKey] =
         GetPackageKeysByProvider( this->m_DetectedProvider );
 
+    const bool bIsTfoPkg = provider == GameProvider::Tfo;
+
+    uc2::PkgFileOptions::ptr_t pOptions;
+
+    if ( bIsTfoPkg == true )
+    {
+        pOptions = uc2::PkgFileOptions::Create();
+        pOptions->SetTfoPkg( true );
+    }
+
     this->m_pPkgFile = uc2::PkgFile::Create(
         this->m_PkgFilePath.filename().generic_string(), this->m_vPkgFileData,
-        CopyViewToNewStr( entryKey ), CopyViewToNewStr( dataKey ) );
+        CopyViewToNewStr( entryKey ), CopyViewToNewStr( dataKey ),
+        pOptions.get() );
 
     if ( this->m_pPkgFile->DecryptHeader() == false )
     {
